e4ecc1fa462a4bf24da4758b7f3dae415c74579c
JaredUNACH <jared.salazar65@unach.mx>
2024-06-01 10:10:05 -0600

    first commit

---
+++ app/views/JS/login.js
@@ -0,0 +1,33 @@
+function togglePasswordVisibility() {
+    var passwordInput = document.getElementById('password');
+    var eyeOpen = document.getElementById('eyeOpen');
+    var eyeSlash = document.getElementById('eyeSlash');
+    var passwordType = passwordInput.getAttribute('type');
+
+    if (passwordType === 'password') {
+        passwordInput.setAttribute('type', 'text');
+        eyeOpen.style.display = '';
+        eyeSlash.style.display = 'none';
+    } else {
+        passwordInput.setAttribute('type', 'password');
+        eyeOpen.style.display = 'none';
+        eyeSlash.style.display = '';
+    }
+}
+
+function showAlert() {
+    var alertDiv = document.createElement('div');
+    alertDiv.className = 'alert';
+    alertDiv.innerHTML = `
+        <i class="fas fa-exclamation-triangle"></i>
+        <p>El usuario y/o la contraseña <br> son incorrectos</p>
+        <button onclick="closeAlert()">Aceptar</button>
+    `;
+    document.body.appendChild(alertDiv);
+}
+
+function closeAlert() {
+    var alertDiv = document.querySelector('.alert');
+    document.body.removeChild(alertDiv);
+    window.location.href = "fondo.html"; 
+}
