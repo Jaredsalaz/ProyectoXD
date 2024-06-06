aef6bd0ed9c3b1e1550159e6599021943346efa9
JaredUNACH <jared.salazar65@unach.mx>
2024-06-06 01:15:02 -0600

    Frontend

--- app/views/JS/menu.js
+++ app/views/JS/menu.js
@@ -56,27 +56,33 @@
   });
   
   
-  // add hovered class to selected list item
-  let list = document.querySelectorAll(".navigation li");
-  
-  function activeLink() {
-    list.forEach((item) => {
-      item.classList.remove("hovered");
-    });
-    this.classList.add("hovered");
-  }
-  
-  list.forEach((item) => item.addEventListener("mouseover", activeLink));
-  
-  // Menu Toggle
-  let toggle = document.querySelector(".toggle");
-  let navigation = document.querySelector(".navigation");
-  let main = document.querySelector(".main");
-  
-  toggle.onclick = function () {
-    navigation.classList.toggle("active");
-    main.classList.toggle("active");
-  };
+// add hovered class to selected list item
+let list = document.querySelectorAll(".navigation li");
+
+function activeLink() {
+  list.forEach((item) => {
+    item.classList.remove("hovered");
+  });
+  this.classList.add("hovered");
+}
+
+list.forEach((item) => item.addEventListener("mouseover", activeLink));
+
+// Menu Toggle
+let toggle = document.querySelector(".toggle");
+let navigation = document.querySelector(".navigation");
+let main = document.querySelector(".main");
+
+// Add 'active' class to navigation and main when the page loads
+window.onload = function() {
+  navigation.classList.add("active");
+  main.classList.add("active");
+};
+
+toggle.onclick = function () {
+  navigation.classList.toggle("active");
+  main.classList.toggle("active");
+};
   
   // Selecciona todos los campos de entrada
 var inputs = document.querySelectorAll('.input input');
